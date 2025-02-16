0c1133403f31dca69d6f11bacf60dd2ca4fd3715
t-manish-kumar <141441980+t-manish-kumar@users.noreply.github.com>
2025-02-16 21:53:29 +0530

    Update WarFileApplication.java

--- src/main/java/com/example/war_file/WarFileApplication.java
+++ src/main/java/com/example/war_file/WarFileApplication.java
@@ -4,8 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@RestController	
 public class WarFileApplication {
 
+	@GetMapping("/helloworld")
+	public String hello() {
+		return "Hello World from WAR";
+	}	
+	
 	public static void main(String[] args) {
 		SpringApplication.run(WarFileApplication.class, args);
 	}
